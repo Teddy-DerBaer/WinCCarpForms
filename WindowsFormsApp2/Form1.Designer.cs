b0261f2c6db06d9df392450319a957fcb4534e15
Balthasar Cronos <balthasarcronos@hotmail.de>
2018-12-27 01:31:11 +0100

    Projektdateien hinzufügen.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 30;
+            this.listBox1.Items.AddRange(new object[] {
+            "d;D;D;F;G;H;J",
+            "eeeeee;dfffff;dfgghhh;yy",
+            "fasdfasdf;asdgasdhsdfjsdf;asdgasdghasdg;ddd",
+            "gfgfg;fdfdddddd;sdsfs"});
+            this.listBox1.Location = new System.Drawing.Point(1330, 196);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1042, 634);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 0;
+            this.listBox1.Enabled = false;
+            this.listBox1.DoubleClick += new System.EventHandler(this.ListBox_DoubleClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(79, 276);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(422, 38);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(703, 227);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(134, 29);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Gersperrt";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(703, 550);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(6);
+            this.checkBox2.MaximumSize = new System.Drawing.Size(198, 190);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(149, 29);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Entsperren";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            this.checkBox2.Click += new System.EventHandler(this.checkBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(698, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Gesperrt muss erst entsperrt werden";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(698, 454);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Sicherheits Sperre";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(222, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "User Namen Einfügen";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1347, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(221, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Auswahl User Gruppe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(2040, 977);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListBox_DoubleClick(object sender, EventArgs e)
+        {
+
+
+            if (checkBox1.Enabled)
+            {
+                comboBox1.Items.Clear();
+                string text = listBox1.SelectedItem.ToString();
+                string[] TArray;
+                TArray = text.Split(';');
+                for (int i = 0; i < TArray.Length; ++i)
+                {
+                    comboBox1.Items.Add(TArray[i]);
+                }
+
+                comboBox1.Enabled = true;
+                checkBox1.Enabled = false;
+                checkBox1.Checked = true;
+                checkBox2.Checked = false;
+                listBox1.Enabled = false;
+            }
+
+
+
+            
+        }
+
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox1.Checked = true;
+                checkBox1.Enabled = false;
+                checkBox2.Checked = false;
+
+            }
+            else
+            {
+                //this->checkBox1->Enabled = false;
+
+                comboBox1.Enabled = true;
+                listBox1.Enabled = true;
+
+            }
+        }
+
+        private void checkBox2_Click(object sender, EventArgs e)
+        {
+
+
+            if (checkBox1.Enabled)
+            {
+
+
+
+            }
+            else
+            {
+
+
+                if (checkBox2.Checked)
+                {
+                    checkBox1.Enabled = true;
+                    //this->checkBox1->Checked = false;
+                }
+                else
+                {
+
+
+                    checkBox1.Enabled = false;
+                    checkBox1.Checked = true;
+                }
+
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
